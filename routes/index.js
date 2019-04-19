71c673d659988dfd2ea60e07d2082a993ea1b1d4
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-19 16:14:37 +0200

    nav and first routes created without styl

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('home.hbs');
+});
+
+/* GET news page */
+router.get('/news', (req, res, next) => {
+  res.render('news.hbs');
+});
+
+/* GET stories page */
+router.get('/stories', (req, res, next) => {
+  res.render('stories.hbs');
+});
+
+/* GET stories page */
+router.get('/big-players', (req, res, next) => {
+  res.render('big-players.hbs');
+});
+
+
+router.get('/discover', (req, res, next) => {
+  res.render('discover.hbs');
+});
+
+
+router.get('/log-in', (req, res, next) => {
+  res.render('log-in.hbs');
 });
 
 module.exports = router;
