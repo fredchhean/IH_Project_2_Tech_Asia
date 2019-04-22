7485790a9ebbfeb1305e26dbdefc69417d58f4c7
Frederic Chhean <chheanfrederic@gmail.com>
2019-04-22 16:34:05 +0200

    read user ok

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,7 +11,9 @@ router.post("/register", (req, res, next) => {
   Profiles.create({ pseudo, password, email })
     //dans then "profile", tu mets ce que tu veux
     .then(profile => {
-      res.redirect("/profile");
+      res.redirect("/profile/" + profile._id);
+      // res.locals.profileInfo = profile
+      // res.render("/profile")
       console.log("wesh new profile created", profile);
     })
     .catch(error => {
@@ -19,18 +21,23 @@ router.post("/register", (req, res, next) => {
     });
 });
 
-//READ USER PROFILE IN profile.hbs
-router.get("/profile", (req, res) => {
-  console.log("ici");
+// READ USER PROFILE IN profile.hbs
+router.get("/profile/:id", (req, res) => {
+  // return console.log(req.params.id)
+  // const { id } = req.params;
+  // console.log("ici");
 
-  // Profiles.findOne({ pseudo })
-  //   .then(profile => {
-  //     res.render({ pseudo, password, email });
-  //     console.log("this is your profile");
-  //   })
-  //   .catch(error => {
-  //     console.log("user not found");
-  //   });
+  Profiles.findById(req.params.id)
+    .then(profile => {
+      res.locals.profileData = profile;
+      res.render("profile.hbs");
+      console.log("this is your profile");
+    })
+    .catch(error => {
+      console.log("user not found");
+    });
+
+    
 });
 
 // Article.find().populate("author") made by Guillaume to reference an author of articles in profiles.js
